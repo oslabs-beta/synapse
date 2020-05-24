54b8a91c073dc6df7ed951d0afa32735a96ad8df
denskarlet <den.skarlet@gmail.com>
2020-05-24 14:25:29 -0400

    commit before merging skeleton

--- build/server/server.js
+++ build/server/server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var PORT = 3000;
 app.get('/', function (req, res) {
-    res.send('hello world');
+  res.send('hello world');
 });
-app.listen(PORT, function () { return console.log("listening on port " + PORT); });
+app.listen(PORT, function () {
+  return console.log('listening on port ' + PORT);
+});
--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,9 @@
 const express = require('express');
+
 const app = express();
 
 const PORT = 3000;
-
 app.get('/', (req, res) => {
   res.send('hello world');
 });
-
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
