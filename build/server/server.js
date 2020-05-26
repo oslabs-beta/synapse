32b21cbd66bca29c2bb92f8b8cb55d26cc52d954
denskarlet <den.skarlet@gmail.com>
2020-05-26 16:45:40 -0400

    commit to resolve

--- build/server/server.js
+++ build/server/server.js
@@ -3,11 +3,10 @@ var express = require('express');
 var synapse = require('./synapse/synapse');
 var app = express();
 var PORT = 3000;
-var test = [];
-for (var _i = 0, test_1 = test; _i < test_1.length; _i++) {
-  var value = test_1[_i];
-}
 app.use('/api', synapse(path.resolve(__dirname, './resources')));
 app.get('/', function (req, res) {
-  res.send('hello world');
+    res.send('hello world');
 });
+app.use(function (err, req, res, next) { return res.status(err.status).send(err.serialize()); });
+app.listen(PORT, function () { return console.log("listening on port " + PORT); });
+module.exports = app;
--- server/fields/Id.ts
+++ server/fields/Id.ts
@@ -1,10 +1,6 @@
 export {};
-<<<<<<< HEAD
-const Text = require('./Text');
-=======
 
-const Text = require("./Text");
->>>>>>> 0ff72496acf4a7b48a69e0b48c48c15ee486f85c
+const Text = require('./Text');
 
 class Id extends Text {
   constructor(length, flags = null) {
@@ -13,7 +9,7 @@ class Id extends Text {
     this.assert(
       /[^\w-]/,
       false,
-      "must contain only alphanumeric characters, underscores and dashes"
+      'must contain only alphanumeric characters, underscores and dashes'
     );
   }
 }
