1f8c55f07a67cf7e9b0cb8fbac4d891441c4b4f4
denskarlet <den.skarlet@gmail.com>
2020-05-29 19:02:50 -0400

    resolving

--- server/synapse/Manager.ts
+++ server/synapse/Manager.ts
@@ -53,11 +53,10 @@ class Manager {
 
     const dependents = this.dependents.get(path);
     dependents.add(client);
+    console.log(dependents);
 
     const subscriptions = this.subscriptions.get(client);
     subscriptions.add(path);
-
-    console.log(this.subscriptions);
   }
 
   /**
@@ -75,6 +74,8 @@ class Manager {
       const dependents = this.dependents.get(target);
       dependents.delete(client);
     });
+
+    console.log(this.subscriptions);
   }
 
   /**
@@ -84,7 +85,6 @@ class Manager {
    * @returns The new value of the resource.
    */
   async update(path: string) {
-    console.log(this.dependents.get(path));
     // the generator always returns an instance of Reply
     const result = await this.generator("get", path);
 
