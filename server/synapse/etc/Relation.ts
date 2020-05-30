3ce91b85ebd12daadfb063cdb9d86654b6bad2d4
Madison Brown <mbrown3391@gmail.com>
2020-05-30 19:25:51 -0400

    refactor

--- server/synapse/etc/Relation.ts
+++ server/synapse/etc/Relation.ts
@@ -1,48 +1,46 @@
-/* eslint-disable no-underscore-dangle */
-
 export default class Relation<F, T> {
-  _from: Map<F, Set<T>> = new Map();
+  fromMap: Map<F, Set<T>> = new Map();
 
-  _to: Map<T, Set<F>> = new Map();
+  toMap: Map<T, Set<F>> = new Map();
 
   link(from: F, to: T) {
-    if (!this._from.has(from)) {
-      this._from.set(from, new Set());
+    if (!this.fromMap.has(from)) {
+      this.fromMap.set(from, new Set());
     }
-    this._from.get(from).add(to);
+    this.fromMap.get(from).add(to);
 
-    if (!this._to.has(to)) {
-      this._to.set(to, new Set());
+    if (!this.toMap.has(to)) {
+      this.toMap.set(to, new Set());
     }
-    this._to.get(to).add(from);
+    this.toMap.get(to).add(from);
   }
 
   unlink(from: F = null, to: T = null) {
     if (from && to) {
-      this._from.get(from).delete(to);
-      this._to.get(to).delete(from);
+      this.fromMap.get(from).delete(to);
+      this.toMap.get(to).delete(from);
     } else if (from) {
-      if (this._from.has(from)) {
-        this._from.get(from).forEach((val: T) => {
-          this._to.get(val).delete(from);
+      if (this.fromMap.has(from)) {
+        this.fromMap.get(from).forEach((val: T) => {
+          this.toMap.get(val).delete(from);
         });
-        this._from.delete(from);
+        this.fromMap.delete(from);
       }
     } else if (to) {
-      if (this._to.has(to)) {
-        this._to.get(to).forEach((val: F) => {
-          this._from.get(val).delete(to);
+      if (this.toMap.has(to)) {
+        this.toMap.get(to).forEach((val: F) => {
+          this.fromMap.get(val).delete(to);
         });
-        this._to.delete(to);
+        this.toMap.delete(to);
       }
     }
   }
 
   from(val: F): Array<T> {
-    return this._from.has(val) ? Array.from(this._from.get(val)) : [];
+    return this.fromMap.has(val) ? Array.from(this.fromMap.get(val)) : [];
   }
 
   to(val: T): Array<F> {
-    return this._to.has(val) ? Array.from(this._to.get(val)) : [];
+    return this.toMap.has(val) ? Array.from(this.toMap.get(val)) : [];
   }
 }
